b03a67b8bafe3d7ad1eff3739c84a16007e6d4e7
dongyeon.kim <dongyeon.kim@lasee.io>
2023-03-31 16:33:34 +0900

    쿼리스트링, 라우팅

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/profiles/:username" element={<Profile />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/articles/:id" element={<Article />} />
         </Routes>
     );
 }
